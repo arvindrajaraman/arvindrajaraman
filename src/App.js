36c4d3e1ff22f8496b77fefd8de4fedfe0939742
Arvind Rajaraman <arvindrajaraman@users.noreply.github.com>
2021-06-26 15:28:29 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import './bootstrap/css/bootstrap.min.css';
+import DigitalWorld from './assets/digitalworld.mp4';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="app">
+            <video className="welcomeVideo" autostart autoPlay muted loop src={DigitalWorld} type="video/mp4" />
+            <div className="welcomePane">
+                <span className="largeText">Hello, I'm <b>Arvind Rajaraman</b>!</span>
+                <br />
+                <span className="smallText">I'm an undergraduate at UC Berkeley's M.E.T. Program, majoring in Electrical Engineering & Computer Science and Business Administration. Here, you will find my endeavors in academia, industry, and more. Welcome!</span>
+            </div>
+            {/*<div className="container">*/}
+            {/*    <div className="row">*/}
+            {/*        <div className="col-sm">def</div>*/}
+            {/*        <div className="col-sm">abc</div>*/}
+            {/*    </div>*/}
+            {/*</div>*/}
+        </div>
+    );
 }
 
 export default App;
---
+++ src/academics/Academics.js
@@ -0,0 +1,11 @@
+import './Academics.css';
+
+function Academics() {
+    return (
+        <div className="academics">
+
+        </div>
+    );
+}
+
+export default Academics;
